7b66326a34e2dedc701af33eaffcca7bc628072e
小心我的贱 <1755596120@qq.com>
2025-02-27 16:31:51 +0800

    ook

--- src/main/java/com/julia/Application.java
+++ src/main/java/com/julia/Application.java
@@ -17,7 +17,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-//        System.out.println(BCrypt.hashpw("porsche"));
+        System.out.println(BCrypt.hashpw("panpan1"));
         SpringApplication.run(Application.class);
     }
 }
