f8ae3f3763cf247e65842efb1de5297f7b4dc56e
小心我的贱 <1755596120@qq.com>
2025-09-16 18:28:45 +0800

    适配骏网 xml

--- src/main/java/com/julia/config/RestTemplateConfig.java
+++ src/main/java/com/julia/config/RestTemplateConfig.java
@@ -21,22 +21,24 @@ import java.util.List;
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
-        RestTemplate restTemplate = new RestTemplate(factory);
-        // 使用 XML 转换器
-        List<HttpMessageConverter<?>> converters = restTemplate.getMessageConverters();
-        converters.removeIf(c -> c instanceof MappingJackson2XmlHttpMessageConverter);
-        converters.add(new MappingJackson2XmlHttpMessageConverter());
-
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        // 获取现有的消息转换器列表
+        List<HttpMessageConverter<?>> messageConverters = restTemplate.getMessageConverters();
+        // 添加 XML 消息转换器
+        messageConverters.add(new MappingJackson2XmlHttpMessageConverter());
+        // 重新设置消息转换器列表
+        restTemplate.setMessageConverters(messageConverters);
         return restTemplate;
+
     }
 
-    @Bean
-    public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(15000);
-        factory.setReadTimeout(5000);
-        return factory;
-    }
+//    @Bean
+//    public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
+//        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+//        factory.setConnectTimeout(15000);
+//        factory.setReadTimeout(5000);
+//        return factory;
+//    }
 
 }
